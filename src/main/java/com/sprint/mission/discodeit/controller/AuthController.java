c46512b0335f93cb99c54f2ed9ed1f699be455bf
parkhamin <parkhamint@naver.com>
2025-12-24 19:01:28 +0900

    feat: AuthController 구현

---
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.request.LoginRequest;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+@ResponseBody
+@RequestMapping("api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    // 사용자는 로그인할 수 있다.
+    // User login(LoginRequest loginRequest);
+    @RequestMapping(value = "/login")
+    public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest){
+        User user = authService.login(loginRequest);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(user);
+    }
+}
