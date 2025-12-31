3c98139e76adcbb2027c59b7705b78e82167ae02
parkhamin <parkhamint@naver.com>
2025-12-31 15:21:27 +0900

    refactor: BinaryContentController를 RESTful API 구조로 개선

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -5,25 +5,21 @@ import com.sprint.mission.discodeit.service.BinaryContentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
-@ResponseBody
-@RequestMapping("/api/binaryContent")
+@RequestMapping("/api/binaryContents")
 public class BinaryContentController {
     private final BinaryContentService binaryContentService;
 
     // 바이너리 파일을 1개 또는 여러 개 조회할 수 있다.
     // BinaryContent find(UUID binaryContentId);
-    @RequestMapping("/find")
-    public ResponseEntity<BinaryContent> find(@RequestParam("binaryContentId") UUID binaryContentId) {
+    @GetMapping("/{binaryContentId}")
+    public ResponseEntity<BinaryContent> find(@PathVariable UUID binaryContentId) {
         BinaryContent binaryContent = binaryContentService.find(binaryContentId);
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -31,7 +27,7 @@ public class BinaryContentController {
     }
 
     // List<BinaryContent> findAllByIdIn(List<UUID> binaryContentIds);
-    @RequestMapping(value = "/findAllByIdIn")
+    @GetMapping
     public ResponseEntity<List<BinaryContent>> findAllByIdIn(@RequestParam("binaryContentIds") List<UUID> binaryContentIds) {
         List<BinaryContent> binaryContents = binaryContentService.findAllByIdIn(binaryContentIds);
         return ResponseEntity
