9c798d9366a87d17604097757e83cce5d7397a04
parkhamin <parkhamint@naver.com>
2025-12-31 15:38:44 +0900

    refactor: ReadStatusController를 RESTful API 구조로 개선

--- src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -7,25 +7,20 @@ import com.sprint.mission.discodeit.service.ReadStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
-@ResponseBody
-@RequestMapping("/api/readStatus")
+@RequestMapping("/api/readStatuses")
 public class ReadStatusController {
     private final ReadStatusService readStatusService;
 
     // 특정 채널의 메시지 수신 정보를 생성할 수 있다.
     // ReadStatus create(ReadStatusCreateRequest readStatusCreateRequest);
-    @RequestMapping(value = "/create")
+    @PostMapping
     public ResponseEntity<ReadStatus> create(@RequestBody ReadStatusCreateRequest readStatusCreateRequest) {
         ReadStatus readStatus = readStatusService.create(readStatusCreateRequest);
         return ResponseEntity
@@ -35,9 +30,9 @@ public class ReadStatusController {
 
     // 특정 채널의 메시지 수신 정보를 수정할 수 있다.
     // ReadStatus update(UUID readStatusId, ReadStatusUpdateRequest readStatusUpdateRequest);
-    @RequestMapping(value = "/update")
+    @PatchMapping(value = "/{readStatusId}")
     public ResponseEntity<ReadStatus> update(
-            @RequestParam("readStatusId") UUID readStatusId,
+            @PathVariable UUID readStatusId,
             @RequestBody ReadStatusUpdateRequest readStatusUpdateRequest) {
         ReadStatus readStatus = readStatusService.update(readStatusId, readStatusUpdateRequest);
         return ResponseEntity
@@ -47,7 +42,7 @@ public class ReadStatusController {
 
     // 특정 사용자의 메시지 수신 정보를 조회할 수 있다.
     // List<ReadStatus> findAllByUserId(UUID userId);
-    @RequestMapping(value = "/findAllByUserId")
+    @GetMapping
     public ResponseEntity<List<ReadStatus>> findAllByUserId(@RequestParam("userId") UUID userId) {
         List<ReadStatus> readStatusList = readStatusService.findAllByUserId(userId);
         return ResponseEntity
