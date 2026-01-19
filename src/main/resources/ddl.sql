6c22926805a1abfc397af500b37ac7ca7f5edb69
parkhamin <parkhamint@naver.com>
2026-01-19 15:00:39 +0900

    fix: binary_contents 테이블 속성 제거

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -14,8 +14,7 @@ CREATE TABLE binary_contents
     created_at   timestamptz  NOT NULL,
     file_name    varchar(255) NOT NULL,
     size         bigint       NOT NULL,
-    content_type varchar(100) NOT NULL,
-    bytes        bytea        NOT NULL
+    content_type varchar(100) NOT NULL
 );
 
 CREATE TABLE users
