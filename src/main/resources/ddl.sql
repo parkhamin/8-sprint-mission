8e8e6b77e847b032533cdd2200b8623defd6ff61
parkhamin <parkhamint@naver.com>
2026-02-03 16:12:44 +0900

    feat: H2 데이터베이스 연동 및 환경별 프로파일 분리

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -14,30 +14,30 @@ DROP TABLE IF EXISTS binary_contents;
 CREATE TABLE channels
 (
     id          uuid PRIMARY KEY,
-    created_at  timestamptz NOT NULL,
-    updated_at  timestamptz,
+    created_at  timestamp with time zone NOT NULL,
+    updated_at  timestamp with time zone,
     name        varchar(100),
     description varchar(500),
-    type        varchar(10) NOT NULL CHECK (type IN ('PUBLIC', 'PRIVATE'))
+    type        varchar(10)              NOT NULL CHECK (type IN ('PUBLIC', 'PRIVATE'))
 );
 
 CREATE TABLE binary_contents
 (
     id           uuid PRIMARY KEY,
-    created_at   timestamptz  NOT NULL,
-    file_name    varchar(255) NOT NULL,
-    size         bigint       NOT NULL,
-    content_type varchar(100) NOT NULL
+    created_at   timestamp with time zone NOT NULL,
+    file_name    varchar(255)             NOT NULL,
+    size         bigint                   NOT NULL,
+    content_type varchar(100)             NOT NULL
 );
 
 CREATE TABLE users
 (
     id         uuid PRIMARY KEY,
-    created_at timestamptz         NOT NULL,
-    updated_at timestamptz,
-    username   varchar(50) UNIQUE  NOT NULL,
-    email      varchar(100) UNIQUE NOT NULL,
-    password   varchar(60)         NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
+    username   varchar(50) UNIQUE       NOT NULL,
+    email      varchar(100) UNIQUE      NOT NULL,
+    password   varchar(60)              NOT NULL,
     profile_id uuid UNIQUE,
 
     CONSTRAINT fk_users_binary_contents
@@ -49,10 +49,10 @@ CREATE TABLE users
 CREATE TABLE user_statuses
 (
     id             uuid PRIMARY KEY,
-    created_at     timestamptz NOT NULL,
-    updated_at     timestamptz,
-    user_id        uuid UNIQUE NOT NULL,
-    last_active_at timestamptz NOT NULL,
+    created_at     timestamp with time zone NOT NULL,
+    updated_at     timestamp with time zone,
+    user_id        uuid UNIQUE              NOT NULL,
+    last_active_at timestamp with time zone NOT NULL,
 
     CONSTRAINT fk_user_statuses_users
         FOREIGN KEY (user_id)
@@ -63,11 +63,11 @@ CREATE TABLE user_statuses
 CREATE TABLE read_statuses
 (
     id           uuid PRIMARY KEY,
-    created_at   timestamptz NOT NULL,
-    updated_at   timestamptz,
-    user_id      uuid        NOT NULL,
-    channel_id   uuid        NOT NULL,
-    last_read_at timestamptz NOT NULL,
+    created_at   timestamp with time zone NOT NULL,
+    updated_at   timestamp with time zone,
+    user_id      uuid                     NOT NULL,
+    channel_id   uuid                     NOT NULL,
+    last_read_at timestamp with time zone NOT NULL,
 
     CONSTRAINT fk_read_statuses_users
         FOREIGN KEY (user_id)
@@ -86,10 +86,10 @@ CREATE TABLE read_statuses
 CREATE TABLE messages
 (
     id         uuid PRIMARY KEY,
-    created_at timestamptz NOT NULL,
-    updated_at timestamptz,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
     content    text,
-    channel_id uuid        NOT NULL,
+    channel_id uuid                     NOT NULL,
     author_id  uuid,
 
     CONSTRAINT fk_messages_channels
